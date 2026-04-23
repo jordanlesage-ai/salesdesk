da9365959fede81952f50a1ab184568fafe3cdef
jordanlesage-ai <jordan.lesage@gmail.com>
2026-04-23 14:20:33 -0400

    commit to main

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(process.env.VITE_CLERK_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || '')
-  }
 })
- 
